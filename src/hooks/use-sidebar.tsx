4848b9e870b4b769d235bc43b194099c08b6cb08
Alejandro <feexeo@gmail.com>
2025-03-10 11:03:08 -0400

    feat: add sidebar base

---
+++ src/hooks/use-sidebar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { SidebarContext } from "@/components/ui/sidebar";
+
+export const useSidebar = () => {
+  const context = React.use(SidebarContext);
+  if (!context) {
+    throw new Error("useSidebar must be used within a SidebarProvider.");
+  }
+
+  return context;
+};
