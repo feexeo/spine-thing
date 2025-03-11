984ff595df74e618fda2661acdc6a0cdc17a3939
Alejandro <feexeo@gmail.com>
2025-03-11 17:09:12 -0400

    refactor: move lintstaged rules to package.json

--- .lintstagedrc.mjs
+++
@@ -1,9 +0,0 @@
-import { resolve } from "path";
-
-export default {
-  "**/*.(ts|tsx|js)": (filenames) => [
-    `pnpm prettier --write ${filenames.map((file) => resolve(file)).join(" ")}`,
-    `pnpm lint . ${filenames.join(" ")}`,
-  ],
-  "*.go": ["gofmt -w"],
-};
